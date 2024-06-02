bd1a61f1eacdb111fa8781644e24b3208b6ad917
Wenlin Fan <67357819+elainekiwi@users.noreply.github.com>
2024-06-02 16:27:36 +0800

    Update navigator.ino

--- PickerMachine/navigator.ino
+++ PickerMachine/navigator.ino
@@ -5,23 +5,43 @@ const int stepXPin = 2; //X.STEP
 const int dirXPin = 5; // X.DIR
 const int stepYPin = 3; //Y.STEP
 const int dirYPin = 6; // Y.DIR
-
 const int pinUpDown = 11;   
+
 int stepPin=stepYPin;
 int dirPin=dirYPin;
 int x_cur=0;
 int y_cur=0;
 const int stepsPerRev=200;
-int pulseWidthMicros = 100; 	// microseconds
-int millisBtwnSteps = 1000;
+int pulseWidthMicros = 70; 	// microseconds
+int millisBtwnSteps = 70;
 void setup() {
  	Serial.begin(9600);
  	pinMode(enPin, OUTPUT);
  	digitalWrite(enPin, LOW);
  	pinMode(stepPin, OUTPUT);
  	pinMode(dirPin, OUTPUT);
+  servoUpDown.attach(11);
 }
 void loop() {
+  for(int i=0;i<3;i++)
+  {
+    int x_read = Serial.parseInt();
+    int y_read = Serial.parseInt();
+    int x=0;
+    int y=0;
+  //calculate the relative distance from the previous point
+  //the picker won't move if there is no input, whose value is by default 0 in the arduino uno case
+ 	  if(x_read!=0||y_read!=0)
+    {
+      x=x_read-x_cur;
+      y=y_read-y_cur;
+      x_cur=x_read;
+      y_cur=y_read;
+    }
+    moveX(x);
+    moveY(y);
+    dip(i);//1-取枪头、点样
+  }
   int x_read = Serial.parseInt();
   int y_read = Serial.parseInt();
   int x=0;
@@ -37,12 +57,9 @@ void loop() {
   }
   moveX(x);
   moveY(y);
-  // Serial.println(x);
-  // Serial.println(y);
-  //not tested
-  //dip();
   // Serial.println(x_cur);
   // Serial.println(y_cur);
+  dip(3);//2-退枪头
   Serial.println("round complete");
   delay(2000);
 }
@@ -97,10 +114,50 @@ void moveX(int x)
  	delay(1000);
 }
 //caution: the code bellow has not been tested yet
-void dip()
+void dip(int mode)
 {
-  servoUpDown.write(150);
-  delay(1000); 
-  servoUpDown.write(90); 
-  delay(1000); 
+  if(mode==0)
+  {
+    servoUpDown.write(60);
+    delay(1000);
+    servoUpDown.write(90);
+    delay(1000);
+    servoUpDown.write(120);
+    delay(1000); 
+    servoUpDown.write(90);
+    delay(1000);
+  }
+  if(mode==1)
+  {
+    servoUpDown.write(60);
+    delay(400);
+    servoUpDown.write(90);
+    delay(1000);
+    servoUpDown.write(120);
+    delay(600); 
+    servoUpDown.write(90);
+    delay(1000);
+  }
+  if(mode==2)
+  {
+    servoUpDown.write(60);
+    delay(400);
+    servoUpDown.write(90);
+    delay(1000);
+    servoUpDown.write(120);
+    delay(600); 
+    servoUpDown.write(90);
+    delay(1000);
+  }
+  else if(mode==3)
+  {
+    servoUpDown.write(120);
+    delay(700);
+    servoUpDown.write(90);
+    delay(1000);
+    servoUpDown.write(60);
+    delay(600); 
+    servoUpDown.write(90);
+    delay(1000);
+  }
 }
